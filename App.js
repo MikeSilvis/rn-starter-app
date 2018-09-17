65d3e1024ee6ee420933164622e0b12cf4e9f154
Mike Silvis <mikesilvis@gmail.com>
2018-09-17 12:48:57 -0700

    ...

--- App.js
+++ App.js
@@ -1,38 +1,20 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    }
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importDefault(require("react"));
-var react_native_1 = require("react-native");
-var App = /** @class */ (function (_super) {
-    __extends(App, _super);
-    function App() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    App.prototype.render = function () {
+const react_1 = __importDefault(require("react"));
+const react_native_1 = require("react-native");
+class App extends react_1.default.Component {
+    render() {
         return (react_1.default.createElement(react_native_1.View, { style: styles.container },
             react_1.default.createElement(react_native_1.Text, null, "AGAIN!"),
             react_1.default.createElement(react_native_1.Text, null, "Changes you make will automatically reload."),
             react_1.default.createElement(react_native_1.Text, null, "Shake your phone to open the developer menu.")));
-    };
-    return App;
-}(react_1.default.Component));
+    }
+}
 exports.default = App;
-var styles = react_native_1.StyleSheet.create({
+const styles = react_native_1.StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
